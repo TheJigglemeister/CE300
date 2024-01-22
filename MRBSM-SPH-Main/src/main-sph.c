896f3c8daf6b9616302fb9a0c5cfa85af3e58cd3
neosapien20 <138009467+neosapien20@users.noreply.github.com>
2024-01-22 19:15:36 +0800

    Revert "Revert "Updated deprecated functions""
    This reverts commit 34be5bcf1d5e8765b530b5ade78f018bfccac872.

--- MRBSM-SPH-Main/src/main-sph.c
+++ MRBSM-SPH-Main/src/main-sph.c
@@ -363,7 +363,7 @@ currently: unit box**/
 
     double x, y;
     FILE*XYCheck;
-    XYCheck = fopen("bindel_XY.csv", "w");
+    XYCheck = fopen_s("bindel_XY.csv", "w");
 
     // Count mesh points that fall in indicated region.
     int count = 0;
@@ -606,7 +606,7 @@ double** main_sph(int argc, char** argv, sim_param_t params, sim_state_t* state,
     int i, count=0;
 
 
-    FILE* fp = fopen(params.fname, "a");
+    FILE* fp = fopen_s(params.fname, "a");
 
     int nframes = params.nframes;
     int npframe = params.npframe;
